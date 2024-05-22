a1af5070c210626d6719ce85019de4d0d2a79ad1
Goemaere Leo <goemaere.leo@hotmail.fr>
2024-05-22 19:36:45 +0200

    Add custom css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: [
+    '~/assets/scss/style.scss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/scss/abstracts/_variables.scss" as *;
+          `
+        }
+      }
+    }
+  },
 })
