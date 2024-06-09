ec6020429222fb86fe7395904c084d0249b91a4f
Goemaere Leo <goemaere.leo@hotmail.fr>
2024-06-10 00:11:38 +0200

    Add description for 531

--- frontend/datas/programs/531/program531.ts
+++ frontend/datas/programs/531/program531.ts
@@ -4,7 +4,7 @@ export const program531: Program = {
   id: crypto.randomUUID(),
   tm_percentage: 0.9,
   name: "531",
-  description: "Description",
+  description: "Le 531 est un programme de force basé sur 4 mouvements principaux, à savoir le bench, soulevé de terre, le squat et la press.",
   variations: [
     TriumvirateVariation
   ]
